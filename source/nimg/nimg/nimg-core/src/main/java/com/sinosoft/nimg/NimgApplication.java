0c00436d21e00612604d67b25ac4b131c01ca819
Guosd <51805808+Guosd@users.noreply.github.com>
2020-02-01 23:01:14 +0800

    Add files via upload

---
+++ source/nimg/nimg/nimg-core/src/main/java/com/sinosoft/nimg/NimgApplication.java
@@ -0,0 +1,14 @@
+package com.sinosoft.nimg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author programmer
+ */
+@SpringBootApplication
+public class NimgApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NimgApplication.class, args);
+    }
+}
