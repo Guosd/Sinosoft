23c4b19f98ea74948765eb704dde766af3fac241
Guosd <1095059264@qq.com>
2020-02-02 12:59:22 +0800

    提交

--- source/nimg/nimg/nimg-core/src/main/java/com/sinosoft/nimg/NimgApplication.java
+++ source/nimg/nimg/nimg-core/src/main/java/com/sinosoft/nimg/NimgApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * @author programmer
+ * @author Guosd
  */
 @SpringBootApplication
 public class NimgApplication {
---
+++ source/nimg/nimg/nimg-core/src/test/java/com/sinosoft/nimg/NimgApplicationTest.java
@@ -0,0 +1,16 @@
+package com.sinosoft.nimg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class NimgApplicationTest {
+
+
+    @Test
+    void contextLoads() {
+        System.out.println("dd"+"test");
+    }
+}
+
